0cb5ba4d6fce4a9300d91f8a02d24e6b69f402b4
Dan Kuehling <dkuehling@district158.org>
2019-09-04 21:16:34 -0500

    Just a little on the top
    More imports to support later functionality
    Changed shebang line
    Comment at the top describing the script

--- mosyle_destiny.py
+++ mosyle_destiny.py
@@ -1,7 +1,18 @@
-#!/usr/local/bin/python3
-import csv
+#!/usr/bin/env python3
+'''
+Find devices in Mosyle Manager export CSV that don't have asset tags set, look those serial numbers up in Destiny, then write a XLSX import file to import back into Mosyle.
+'''
+# Standard Imports
+import json
+import argparse
+import subprocess
+import logging
 import os
 import sys
+import re
+# Custom Imports
+import pytds
+import csv
 
 def read_serials_from_csv(csv_file):
     serials = []
@@ -15,11 +26,12 @@ def read_serials_from_csv(csv_file):
 
 
 
+
 def main():
     if len(sys.argv) == 2:
         csv_file = os.path.abspath(sys.argv[1])
         serials = read_serials_from_csv(csv_file)
-        
+
     sys.exit(0)
 
 if __name__ == '__main__':
